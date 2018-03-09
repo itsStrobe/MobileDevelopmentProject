616982f7d515e6a8cb4f356ce6f3ed1bf3ffb198
Alumno <alumno@laboratorios-macbook-pro-95.local>
2018-03-09 15:52:16 -0600

    Added Core Data.

---
+++ ProyectoFinal - Organizador de Apuntes/CourseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CourseTableViewCell.swift
+//  Organizador De Apuntes
+//
+//  Created by Alumno on 3/9/18.
+//  Copyright © 2018 itesm. All rights reserved.
+//
+
+import UIKit
+
+class CourseTableViewCell: UITableViewCell {
+    
+    var nomCurso : String!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
--- ProyectoFinal - Organizador de Apuntes/ViewController.swift
+++ ProyectoFinal - Organizador de Apuntes/ViewController.swift
@@ -8,18 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Cursos"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableView.dequeueReusableCell(withIdentifier: "cellCourse")!
+        return celda
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(50)
+    }
 
 }
 
